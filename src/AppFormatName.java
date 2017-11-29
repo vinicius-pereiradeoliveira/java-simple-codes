0075b4f5f53b6b62f5d58014fe2c221968a160ea
vinicius.oliveira <vinicius.oliveira@ilegra.com>
2017-11-29 11:55:41 -0200

    add comentário

--- src/AppFormatName.java
+++ src/AppFormatName.java
@@ -4,7 +4,7 @@ public class AppFormatName {
 
 	public static void main(String[] args) {
 		String inputName = "Vinicius Pereira Oliveira da Silva Ramos";
-		
+		// Chama metodo para abreviar String de nome
 		FormatService formatService = new FormatService();
 		System.out.println(formatService.formatName(inputName));
 	}
